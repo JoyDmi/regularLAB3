6bcdf5ca9d6b0d96cb23c3fc48f1bc0484709676
JoyDmi <demasya13@gmail.com>
2022-02-27 22:35:29 +0200

    Task2A

---
+++ task-2/task-2A/task-2A.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Task 2 (a)</title>
+</head>
+<body>
+<form name="textStats" action="text-2A.php" method="POST">
+    <label>
+                <textarea name="textBody" rows="20" cols="150"></textarea>
+    </label>
+    <br>
+    <input type="submit" name="analyzeBtn" value="Analyze" />
+</form>
+</body>
+</html><?php
